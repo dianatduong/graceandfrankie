97e44d24a289d86dc8c33eb5c024c42b8f9068be
Diana Duong <dduong013@gmail.com>
2016-03-28 13:22:48 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	var startButton = $('.start-button')
+	var mainIntro = $('.main-intro')
+
+
+	$('.start-button').click(function(){
+		mainIntro.hide()
+	})
+
+
+
+})
