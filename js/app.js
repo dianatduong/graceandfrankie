c4beaebe86e151cf1ef0c45b07835b1f665746d2
Diana Duong <dduong013@gmail.com>
2016-03-30 14:25:09 -0500

    updated added generateQuestion function

--- js/app.js
+++ js/app.js
@@ -2,105 +2,57 @@
 
 $(document).ready(function() {
 
-	var startButton = $('.start-button')
-	var mainIntro = $('.main-intro')
-	var startGame = $('.game')
-	var feedbackHeader = $('.feedback-header')
-	var question = $('.question')
-	var optionButtons = $('.option-btn')
-	var count = $('#question-number')
-	var questionNumber = 1
-	var nextDiv = $('.next')
-	var nextButton = $('#next-btn')
+  
+
+  var beginQuiz = $('.quiz-wrapper')
+  var feedbackHeader = $('.feedback-header')
+
+  var questionDiv = $('.question')
+  var choicesDiv = $('.choices')
+
+  var count = $('span#question-number')
+  var questionNumber = 1
+
+  var nextDiv = $('.next')
+  var nextButton = $('#next-btn')
 
 
-	// array of questions
-	var quizQuestions = [
-		"\"Your anger is frightening the sand.\"",
-		"\"If anyone is going to sit on Ryan Gosling's face, it's gonna be me!\"",
-		"\"That's a lubricant?! I've been putting that on my toast.\"",
-		"\"Take your scissors away from my rug\"",
-		"\"Oh! That is the worst iced tea ever! What is in there, ass?!\""
-	]
+// hides the quiz to display intro
+beginQuiz.hide()
 
-
-	// startGame.hide()
-
-
-	// $('.start-btn').click(function(){
-	// 	mainIntro.hide()
-	// 	startGame.show()
-	// })
-
-	
-
-	feedback(quizQuestions[0], frankieClickedTrue())
-	feedback(quizQuestions[0], graceClickedFalse())
+// when user clicks start button, the intro hides and quiz displays
+$('.start-btn').click(function() {
+	$('.main-intro').hide()
+	beginQuiz.show()
+	generateQuestions()
+})
 
 
 
-		// $('#next-btn').click(function() {
-		// 	console.log("next button clicked")
-		// 	optionButtons.show()
-		// 	questionNumber++;
-  //    		count.text(questionNumber)
-  //    		feedback().remove()
-		// })
-
-	function feedback (index, userAnswer) {
-		return true;
+// function to generate questions
+function generateQuestions() {
+	//array of questions
+	var q = [{
+	    question: "\"Your anger is frightening the sand.\""
+	}, {
+	    question: "\"If anyone is going to sit on Ryan Gosling's face, it's gonna be me!\""
+	}, {
+	    question: "\"That's a lubricant?! I've been putting that on my toast.\""
+	}, {
+	    question: "\"Take your scissors away from my rug\""
+	}, {
+	    question: "\"Oh! That is the worst iced tea ever! What is in there, ass?!\""
+	  }]
+	// loop through questions
+	for (var i = 0; i < 1; i++) {
+	  var quiz = q[i];
+	    questionDiv.append(quiz.question);
 	}
-	
-	function frankieClickedTrue() {
-		$('#frankie-btn').click(function() {
-			console.log("frankieTrue clicked")
-			answerTrue()
-		})
-	}
-
-	function frankieClickedFalse() {
-		$('#frankie-btn').click(function() {
-			console.log("frankiefalse clicked")
-			answerFalse()
-		})
-	}
-
-	function graceClickedTrue() {
-		$('#grace-btn').click(function() {
-			console.log("graceTrue clicked")
-			answerTrue()
-		})
-	}
-
-	function graceClickedFalse() {
-		$('#grace-btn').click(function() {
-			console.log("graceFalse clicked")
-			answerFalse()
-		})
-	}
-	
-	function answerTrue() {
-		feedbackHeader.text("Yay!")
-		question.text("You are correct!")
-		optionButtons.empty()
-		nextDiv.append('<button id="next-btn" class="button">Next</button>')
-
-	}
-	
-	function answerFalse() {
-		feedbackHeader.text("Sorry.")
-		question.text("That is not the correct answer.")
-		optionButtons.empty()
-		nextDiv.append('<button id="next-btn" class="button">Next</button>')
-	}
-
-
-	
-	
-	
-
-
+}
 
 })
+
+
+
 
 	
